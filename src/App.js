3fd888c3a2163c7a7baa291a7346992af8048f0a
ingvillajimenez <ing.villa.jimenez@gmail.com>
2019-06-22 14:34:44 -0500

    react router done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+// Material-UI
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
 import Fab from '@material-ui/core/Fab';
-import Icon from '@material-ui/core/Icon';
+import AddIcon from '@material-ui/icons/Add';
+// Notes Components
+import NotesForm from './NotesForm';
+import NotesList from './NotesList';
+import Home from './Home';
+import Note from './Note';
+// React Router
+import { Link, Route } from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props) {
@@ -21,6 +28,23 @@ class App extends React.Component {
     });
   };
 
+  saveNote = () => {
+    if (this.state.title && this.state.description) {
+      this.setState({
+        notes: [
+          ...this.state.notes,
+          { 
+            id: Date.now(),
+            title: this.state.title, 
+            description: this.state.description 
+          }
+        ],
+        title: '',
+        description: ''
+      });
+    }
+  };
+
   render() {
     console.log(this.state);
     return (
@@ -28,37 +52,47 @@ class App extends React.Component {
         <Typography align='center' variant='h2' gutterBottom>
           My Notes
         </Typography>
+
+        {/* <Link to='/home'>Home</Link>
+        <Link to='/about-us'>About Us</Link>
+        <Link to='/contact'>Contact</Link> */}
+
+        {/* <a href="/home">Home</a> */}
+
         <Grid container justify="center" spacing={2}>
+          <Grid item xs={4}>
+            <NotesList notes={this.state.notes} />
+          </Grid>
           <Grid item xs={8}>
-            <Grid item xs={12}>
-              <TextField 
-                type='text'
-                placeholder='Title for this note...'
-                margin='normal'
-                fullWidth
-                value={this.state.title}
-                onChange={(e) => {this.updateField('title')(e)}}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField 
-                placeholder='Start taking notes...'
-                margin='normal'
-                multiline
-                rows='4'
-                fullWidth
-                value={this.state.description}
-                onChange={this.updateField('description')}
-              />
-            </Grid>
-            <Fab color='secondary'>
-              <Icon>edit_icon</Icon>
-            </Fab>
+            <Route exact path="/" component={Home} />
+            <Route 
+              path="/new" 
+              render={() => (
+                <NotesForm 
+                  title={this.state.title}
+                  description={this.state.description}
+                  updateField={this.updateField}
+                  saveNote={this.saveNote}
+                />      
+              )}
+            />
+            <Route 
+              path="/view/:id"
+              render={props => <Note {...props} notes={this.state.notes} />} 
+            />
           </Grid>
         </Grid>
+        <Fab color="primary" component={Link} to="/new">
+          <AddIcon />
+        </Fab>
       </React.Fragment>
     )
   }
 }
 
 export default App;
+
+// Componentes importantes en react router
+// -BrowserRouter
+// -Link (sin rediriguir a diferencia de la etiqueta a cambiando la url pero queda estatico en la aplicacion)
+// -Route
---
+++ src/NotesList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { Link } from 'react-router-dom';
+
+const NotesList = ({ notes }) => {
+  return(
+    <List>
+      {notes.length ? (
+        notes.map((note, index) => (
+        /* material-ui permite pasar el component 'Link' de react-router por medio de la prop 'component' */
+        <ListItem key={index} button component={Link} to={`/view/${note.id}`} >
+          <ListItemText primary={note.title} />
+          <ListItemSecondaryAction>
+            <IconButton>
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      ))
+    ) : (
+      <Typography align="center" variant="subtitle1">
+        No notes yet...
+      </Typography>
+    )}
+    </List>
+  );
+};
+
+export default NotesList;
