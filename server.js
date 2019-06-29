0a5d6769fadce90908a90c668261ef6a955026b5
ingvillajimenez <ing.villa.jimenez@gmail.com>
2019-06-29 11:48:49 -0500

    deploy

---
+++ server.js
@@ -0,0 +1,24 @@
+// Generar version de produccion
+// heroku --version
+// heroku login
+// heroku create
+// npm i -S express path
+// npm run build (si se modifica algo de public y src se corre de nuevo npm run build)
+// node server.js
+// se modifica el script del package.json "start": "react-scripts start" por "start": "node server.js"
+// npm run start
+// git remote add heroku https://git.heroku.com/dry-fortress-18216.git (si no tengo el remote)
+
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port);
